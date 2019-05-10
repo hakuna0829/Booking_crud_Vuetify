2fd965f6c487004a3fea47680691f034a64e0c74
hakuna0829 <hakuna0829@gmail.com>
2019-05-10 19:07:48 +0700

    Add

--- src/router.js
+++ src/router.js
@@ -1,26 +1,83 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import store from "./store";
+import VueSession from "vue-session";
+
+import Login from "./components/auth/Login.vue";
+import Register from "./components/auth/Register.vue";
+
+// book router
+import Book from "./components/books/Index.vue";
+
+// cart router
+import Cart from "./components/cart/Index.vue";
 
 Vue.use(Router);
 
-export default new Router({
+let router = new Router({
   mode: "history",
-  base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/login",
+      name: "login",
+      component: Login,
+      meta: {
+        requireSession: true
+      }
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/register",
+      name: "register",
+      component: Register,
+      meta: {
+        requireSession: true
+      }
+    },
+    {
+      path: "/books",
+      name: "books",
+      component: Book,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      component: Cart,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+const options = {
+  persist: true
+}
+
+Vue.use(VueSession, options);
+const session = Vue.prototype.$session;
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireSession)) {
+    if (session.exists()) {
+        next("/books");
+        return;
+    }
+    next();
+  } 
+  else if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    }
+    next("/login");
+  } 
+  else {
+    next();
+  }
+});
+
+export default router;
